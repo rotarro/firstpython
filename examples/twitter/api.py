53e7e2cb743359e4fb659b8c38dc39a7a13141ac
tree (나무) <reaperes@coupang.com>
2017-10-01 23:01:34 +0900

    twitter api example

---
+++ examples/twitter/api.py
@@ -0,0 +1,19 @@
+import tweepy
+
+consumer_key = 'G9JzFD2FymO3h5dpvG52zKe5H'
+consumer_secret = 'CyRqDLpi2fW7ttCsfGBusLH5oJdXlaRBD4zUpFr2v010nMnTEN'
+
+access_token = '208308487-ezP0oQxgDPc36btkQkDoAN1hWOBUi8HYEElTn5ES'
+access_token_secret = 'uqXgze2pfJQVIOEMnxcTZNKzlT2Yt1GQWARSu6SJUOqP4'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+search_keyword = '무뚝뚝감자칩'
+
+tweets = api.search(search_keyword)
+for tweet in tweets:
+    print(tweet.text)
+
